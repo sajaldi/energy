87069b2f2f341ed97d068df02a8f34c190c04e6b
sajaldi <saul.alvarado@outlook.com>
2025-07-02 11:05:10 -0600

    actualización versión 1.1

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,28 @@
 from django.urls import path
 from . import views
 
+# Esta línea es la que DEFINE el namespace 'core' para este conjunto de URLs.
+app_name = 'core'
+
 urlpatterns = [
+    # Ruta para la página de inicio
+    
     path('import-consumo/', views.import_excel, name='import_consumo'),
     path('admin-menu/', views.admin_menu, name='admin_menu'),  # New entry
+    # Ruta para el reporte mensual
+    path(
+        'reportes/consumo-mensual/', 
+        views.reporte_consumo_mensual, 
+        name='reporte_consumo_mensual'
+    ),
+    
+    # Ruta para la importación
+    path('import-consumo/', views.import_excel, name='import_consumo'),
+
+    # Ruta para el detalle diario AJAX
+    path(
+        'reportes/detalle-diario/<int:medidor_id>/<str:mes_str>/',
+        views.reporte_consumo_diario,
+        name='reporte_detalle_diario_ajax'
+    ),
 ]
--- energia/urls.py
+++ energia/urls.py
@@ -22,7 +22,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),
+    path('', include('core.urls', namespace='core')),
+    
+    
 ]
 
 # Asegúrate de agregar las rutas estáticas en modo desarrollo
