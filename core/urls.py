ffbdb1b01f340f45f44aae8e0171b0cb4bc09067
sajaldi <36281849+sajaldi@users.noreply.github.com>
2025-07-03 10:43:01 -0600

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -6,9 +6,9 @@ app_name = 'core'
 
 urlpatterns = [
     # Ruta para la página de inicio
-    path('import-consumo/', views.import_excel, name='import_consumo'),
-    path('reportes/consumo-mensual/', views.reporte_consumo_mensual, name='reporte_consumo_mensual'),
+    ##path('import-consumo/', views.import_excel, name='import_consumo'),
+    ##path('reportes/consumo-mensual/', views.reporte_consumo_mensual, name='reporte_consumo_mensual'),
     # Ruta para la importación
-    path('reportes/detalle-diario/<int:medidor_id>/<str:mes_str>/',views.reporte_consumo_diario,name='reporte_detalle_diario_ajax'
+    ##path('reportes/detalle-diario/<int:medidor_id>/<str:mes_str>/',views.reporte_consumo_diario,name='reporte_detalle_diario_ajax'
     ),
 ]
