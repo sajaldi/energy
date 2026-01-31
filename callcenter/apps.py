c8c229ebba924782a27a9f9b588493e9cbc42857
sajaldi <saul.alvarado@outlook.com>
2026-01-30 20:53:34 -0600

    redis<

--- callcenter/apps.py
+++ callcenter/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class CallcenterConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'callcenter'
+    verbose_name = 'MAO'
--- callcenter/views.py
+++ callcenter/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib import messages
+from .tasks import sync_tickets_task
+from django.contrib.admin.views.decorators import staff_member_required
 
-# Create your views here.
+import logging
+logger = logging.getLogger(__name__)
+
+@staff_member_required
+def trigger_sync_tickets(request):
+    """
+    Vista para activar la sincronización de tickets de forma manual.
+    """
+    days = int(request.GET.get('days', 2))
+    
+    try:
+        logger.info(f"[DEBUG] Despachando tarea sync_tickets_task para los últimos {days} días...")
+        sync_tickets_task.delay(days=days)
+        logger.info("[DEBUG] Tarea despachada correctamente a Celery.")
+        messages.success(request, f"Se ha iniciado la sincronización de los últimos {days} días en segundo plano.")
+    except Exception as e:
+        logger.error(f"[ERROR] No se pudo enviar la tarea a Celery: {e}")
+        messages.error(request, f"Error al iniciar la sincronización: {e}")
+    
+    # Redirigir al listado de tickets en el admin
+    return redirect('admin:callcenter_solicitudticket_changelist')
