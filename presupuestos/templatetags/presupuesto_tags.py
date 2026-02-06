939062b0f08bd76ca33be57d0f1b238641f23200
sajaldi <saul.alvarado@outlook.com>
2026-02-06 12:57:38 -0600

    Formateo de moneda: Punto para decimales y coma para miles

--- presupuestos/templatetags/presupuesto_tags.py
+++ presupuestos/templatetags/presupuesto_tags.py
@@ -34,3 +34,21 @@ def index(sequence, position):
 def addstr(arg1, arg2):
     """concaterate arg1 & arg2"""
     return str(arg1) + str(arg2)
+
+@register.filter
+def currency(value):
+    """Format decimal as currency: 1,234.56"""
+    try:
+        import locale
+        # locale.setlocale(locale.LC_ALL, 'en_US.UTF-8') # Removed to avoid platform issues
+        return "{:,.2f}".format(float(value))
+    except (ValueError, TypeError):
+        return value
+
+@register.filter
+def percentage(value):
+    """Format decimal as percentage: 50.00%"""
+    try:
+        return "{:,.2f}%".format(float(value))
+    except (ValueError, TypeError):
+        return value
