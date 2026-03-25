57dc4f51d2aafcca84616864e744bc684ee9a77f
sajaldi <saul.alvarado@outlook.com>
2026-03-25 17:21:20 -0600

    Fix: Add missing get_object_or_404 import in views.py

--- callcenter/views.py
+++ callcenter/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect, get_object_or_404
 from django.contrib import messages
 from .tasks import sync_tickets_task
 from django.contrib.admin.views.decorators import staff_member_required
